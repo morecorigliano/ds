552e908e6a17e3dcdb24470fa2721f88aa764c6e
morena <morena@dsklabs.com>
2020-08-03 05:17:01 -0300

    avance 31/7

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 160,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#423652"
+        "value": "#ffffff"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#39738C"
+          "color": "#000000"
         },
         "polygon": {
           "nb_sides": 5
@@ -28,43 +28,43 @@ particlesJS('particles-js',
         }
       },
       "opacity": {
-        "value": 0.3,
-        "random": false,
+        "value": 1,
+        "random": true,
         "anim": {
-          "enable": false,
+          "enable": true,
           "speed": 1,
-          "opacity_min": 0.1,
+          "opacity_min": 0,
           "sync": false
         }
       },
       "size": {
-        "value": 4,
+        "value": 3,
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
+          "speed": 4,
+          "size_min": 0.3,
           "sync": false
         }
       },
       "line_linked": {
-        "enable": true,
-        "distance": 400,
-        "color": "#39738C",
-        "opacity": 0.3,
+        "enable": false,
+        "distance": 150,
+        "color": "#ffff",
+        "opacity": 0.4,
         "width": 1
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 1,
         "direction": "none",
-        "random": false,
+        "random": true,
         "straight": false,
         "out_mode": "out",
         "attract": {
           "enable": false,
           "rotateX": 600,
-          "rotateY": 1200
+          "rotateY": 600
         }
       }
     },
@@ -73,7 +73,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "repulse"
+          "mode": "bubble"
         },
         "onclick": {
           "enable": true,
@@ -89,14 +89,14 @@ particlesJS('particles-js',
           }
         },
         "bubble": {
-          "distance": 400,
-          "size": 40,
+          "distance": 250,
+          "size": 0,
           "duration": 2,
-          "opacity": 8,
+          "opacity": 0,
           "speed": 3
         },
         "repulse": {
-          "distance": 200
+          "distance": 0.4
         },
         "push": {
           "particles_nb": 4
